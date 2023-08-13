af05b04f2fd0139e99fd4714c95b445f2375ad03
vupadhayayx86 <vupadhayayx86@gmail.com>
2023-08-13 10:50:38 +0530

    Initial Project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="container">
+      <nav>
+        navbar
+      </nav>
+      <main>
+        main content
+      </main>
+      <footer>
+        footer
+      </footer>
+    </div>
+  )
+}
+
+export default App
